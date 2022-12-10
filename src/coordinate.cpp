96ca81f586264639c6a0e846df34d9aafa55034e
ttnguy19 <114105118+ttnguy19@users.noreply.github.com>
2022-12-10 14:14:13 -0500

    Added coordinate.cpp and coordinate.h

---
+++ src/coordinate.cpp
@@ -0,0 +1,25 @@
+
+#include "coordinate.h"
+Coordinate::Coordinate()
+{
+            x=0;
+            y=0;
+
+}
+Coordinate::Coordinate(int x_in,int y_in)
+{
+            x=x_in;
+            y=y_in;
+}
+double Coordinate::GetX(){
+    return x;
+}
+double Coordinate::GetY(){
+    return y;
+}
+ostream &operator<<(ostream &out, Coordinate &c1){
+    out<<"("<<c1.x<<","<<c1.y<<")"<<endl;
+    return out;
+}
+
+
---
+++ src/coordinate.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <ostream>
+#pragma once
+using namespace std;
+class Coordinate{
+    public:
+        int x;
+        int y;
+        Coordinate();
+        Coordinate(int x_in,int y_in);
+        double GetX();
+        double GetY();
+    
+};
+ostream &operator<<(ostream &out,  Coordinate &c1);
